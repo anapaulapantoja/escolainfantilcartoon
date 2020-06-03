d2f7391f75fdd917c14bddac24bd492c0b767ffe
anapaulapantoja <66228848+anapaulapantoja@users.noreply.github.com>
2020-06-03 20:56:18 -0300

    Add files via upload

---
+++ editar-usuario (2).php
@@ -0,0 +1,28 @@
+<h1>Editar Usuário</h1>
+<?php
+	$sql = "SELECT * FROM usuarios WHERE id_usuario=".$_REQUEST["id_usuario"];
+	$res = $conn->query($sql);
+	$row = $res->fetch_object();
+?>
+<form action="?page=salvar-usuario" method="POST">
+	<div class="form-group">
+		<label>Nome</label>
+		<input type="text" name="nome" class="form-control" value="<?php print $row->nome; ?>" required >
+	</div>
+	<div class="form-group">
+		<label>Email</label>
+		<input type="email" name="email" class="form-control" value="<?php print $row->email; ?>" required >
+	</div>
+	<div class="form-group">
+		<label>Username</label>
+		<input type="text" name="usuario" class="form-control" value="<?php print $row->usuario; ?>" required >
+	</div>
+	<div class="form-group">
+		<input type="hidden" name="acao" value="editar">
+		<input type="hidden" name="id_usuario" value="<?php  print $row->id_usuario; ?>">
+		<button type="submit" class="btn btn-success">
+			Editar
+		</button>
+		<a class="btn btn-success" href="secretaria.php" role="button">Voltar</a>
+	</div>
+</form>
